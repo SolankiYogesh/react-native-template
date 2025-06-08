1c9e1e04a75258c658e265e5aeff57e6cd2c7559
Yogesh Solanki <70507671+SolankiYogesh@users.noreply.github.com>
2025-06-08 12:50:23 +0530

    chore: type clean up

--- template/src/Types/screen.types.ts
+++
@@ -1,8 +0,0 @@
-import type {RouteProp} from '@react-navigation/native'
-
-import type {RootStackParamList} from './types'
-
-export type RootRouteProps<RouteName extends keyof RootStackParamList> = RouteProp<
-  RootStackParamList,
-  RouteName
->
